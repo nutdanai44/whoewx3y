459bd8b91feef2e08dd554d468186d7c0470444c
Whoewx3Y <>
2018-09-07 23:58:52 +0700

    init class

---
+++ Whoewx3y/Whoewx3y/Whoewx3yFunction.swift
@@ -0,0 +1,19 @@
+//
+//  CommonFunction.swift
+//  Whoewx3y
+//
+//  Created by Nutdanai Charoenpanichsanti on 7/9/2561 BE.
+//  Copyright © 2561 Nutdanai Charoenpanichsanti. All rights reserved.
+//
+
+import Foundation
+
+public class Whoewx3yFunction {
+    private init() {
+        
+    }
+    
+    public static func doSomething() -> String {
+        return "Yes you got it"
+    }
+}
